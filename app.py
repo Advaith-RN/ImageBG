1ee36f75eb27d99e7889fb9bf0eeae22cd140b0f
RazerAds <adunair123@gmail.com>
2024-03-16 20:01:59 +0530

    Prep for deployment

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def upload_file():
 
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5100)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=True, port=5100)
